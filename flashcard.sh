7dcedcd0fd1e6d4c328a66e08a008ba6ae15de56
_N3m0 <n3m0o7@hotmail.com>
2023-12-24 15:11:18 +0100

    init: get + edit

---
+++ flashcard.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+carddir=/home/cptbb/dev/flashcard/card
+
+first_interval="1"
+step="2"
+
+bold="\033[1m"
+underline="\033[4m"
+nocolor="\033[0m"
+
+case $1 in
+  "")
+    tree -d $carddir
+    ;;
+
+  "list")
+    ls $carddir/$2
+    ;;
+
+  "get")
+    case $2 in
+      "recto" | "verso" | "int")
+        cat $carddir/$3.$2
+        ;;
+      *)
+        echo -e $bold$underline"interval:"$nocolor
+        cat $carddir/$2.int
+
+        echo -e $bold$underline"recto:"$nocolor
+        cat $carddir/$2.recto
+
+        echo -e $bold$underline"verso:"$nocolor
+        cat $carddir/$2.verso
+        ;;
+    esac
+    ;;
+
+  "edit")
+    case $2 in
+      "recto")
+        if [[ $4 == "from" ]]; then
+          cp $5 $carddir/$3.recto
+        else
+          echo $4 > $carddir/$3.recto
+        fi
+
+        [ -f $carddir/$3.int ] || {
+          echo $first_interval > $carddir/$3.int
+        }
+        ;;
+
+      "verso")
+        if [[ $4 == "from" ]]; then
+          cp $5 $carddir/$3.verso
+        else
+          echo $4 > $carddir/$3.verso
+        fi
+
+        [ -f $carddir/$3.int ] || {
+          echo $first_interval > $carddir/$3.int
+        }
+        ;;
+
+      *)
+        if [[ "$3" == "from" && "$5" == "from" ]]; then
+          cp $4 $carddir/$2.recto
+          cp $6 $carddir/$2.verso
+        elif [[ "$3" == "from" ]]; then
+          cp $4 $carddir/$2.recto
+          echo $5 > $carddir/$2.verso
+        elif [[ "$4" == "from" ]]; then
+          echo $3 > $carddir/$2.recto
+          cp $5 $carddir/$2.verso
+        else
+          echo $3 > $carddir/$2.recto
+          echo $4 > $carddir/$2.verso
+        fi
+
+        [ -f $carddir/$2.int ] || {
+          echo $first_interval > $carddir/$2.int
+        }
+        ;;
+      esac
+    ;;
+
+  *)
+    echo "learned"
+    ;;
+esac
